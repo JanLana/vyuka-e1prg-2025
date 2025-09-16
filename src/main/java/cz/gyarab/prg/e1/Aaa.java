591add1664f7778e5bfb310c14043ed3142f20b4
Jan Lana <jan.lana@gyarab.cz>
2025-09-16 09:19:37 +0200

    dnesni hodina

---
+++ src/main/java/cz/gyarab/prg/e1/Aaa.java
@@ -0,0 +1,16 @@
+package cz.gyarab.prg.e1;
+
+public class Aaa {
+    public static void main(String[] args) {
+
+        // vypis prvni radek
+        System.out.println("============================");
+
+        for (int i = 0; i < 3; i = i + 1) {
+            System.out.println("Když dospěla lenost k mozku,");
+            System.out.println("už je pozdě na rákosku.");
+        }
+
+    }
+}
+
---
+++ src/main/java/cz/gyarab/prg/e1/Hvezdicka.java
@@ -0,0 +1,10 @@
+package cz.gyarab.prg.e1;
+
+public class Hvezdicka {
+    public static void main(String[] args) {
+        System.out.print("**********");
+        System.out.print("1111111111");
+        System.out.println("22222");
+        System.out.print("333");
+    }
+}
