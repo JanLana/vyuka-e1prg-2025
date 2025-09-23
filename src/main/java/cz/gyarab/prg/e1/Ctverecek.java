b1188df6ea9854d3ba56f13dbbf9ef6e2283917b
Jan Lana <jan.lana@gyarab.cz>
2025-09-23 09:24:25 +0200

    dnesni hodina

---
+++ src/main/java/cz/gyarab/prg/e1/Ctverecek.java
@@ -0,0 +1,13 @@
+package cz.gyarab.prg.e1;
+
+public class Ctverecek {
+    public static void main(String[] args) {
+        System.out.println("+ + + + +");
+
+        int i;
+        for (i = 0; i < 3; i++) {
+            System.out.println("+       +");
+        }
+        System.out.println("+ + + + +");
+    }
+}
---
+++ src/main/java/cz/gyarab/prg/e1/Rady.java
@@ -0,0 +1,25 @@
+package cz.gyarab.prg.e1;
+
+public class Rady {
+    public static void main(String[] args) {
+
+        int i;
+
+
+        long soucet;
+        soucet = 1;
+
+        for (i = 1 ; i <= 100; i = i + 1) { // i++ -> i = i + 1
+            // tady sectu neco s necim .fdslflkdsajfdlka System.out.println(i);
+            soucet = soucet * i;
+
+            System.out.print(i);
+            System.out.print(":");
+            System.out.println(soucet);
+
+        }
+
+        System.out.println(soucet);
+    }
+
+}
