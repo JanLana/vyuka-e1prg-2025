65fa01d4e8c7462eecca987658efa861a6ab74c7
Jan Lana <jan.lana@gyarab.cz>
2025-11-04 11:21:27 +0100

    dnesni hodina

---
+++ src/main/java/cz/gyarab/prg/e1/s2/PoleTri.java
@@ -0,0 +1,21 @@
+package cz.gyarab.prg.e1.s2;
+
+
+import java.util.Random;
+
+public class PoleTri {
+    public static void main(String[] args) {
+        Random r = new Random();
+
+        int[] arr;
+        arr = new int[13];
+
+        for (int i = 0; i < 13; i++) {
+            arr[i] = r.nextInt(1001);
+        }
+
+        for (int i = 0; i < 13; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
