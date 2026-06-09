09cdc9b3bf3465b7202d1187acd641f06d427f9e
Jan Lana <jan.lana@gyarab.cz>
2026-06-09 11:24:49 +0200

    dnesni hodina

--- src/main/java/cz/gyarab/prg/e1/s2/PrectiBibli.java
+++ src/main/java/cz/gyarab/prg/e1/s2/PrectiBibli.java
@@ -2,38 +2,73 @@ package cz.gyarab.prg.e1.s2;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.security.spec.RSAOtherPrimeInfo;
+import java.util.*;
 
 public class PrectiBibli {
     public static void main(String[] args) {
         String jmenoSouboru = "complet.txt";
         File soubor = new File(jmenoSouboru);
         Scanner sc = null;
-        ArrayList arr = new ArrayList<>();
 
+        Set seznamSlov = new HashSet();
+
+        long casStart = System.currentTimeMillis();
+        long pocetSlov = 0;
         try {
             sc = new Scanner(soubor);
 
             while (sc.hasNext()) {
+                pocetSlov++;
                 String slovo = sc.next();
+                seznamSlov.add(slovo);
 
-                if (uzJsmeTohleSlovoVideli(slovo, arr) == false) {
-                    arr.add(slovo);
-                }
+                //if (uzJsmeTohleSlovoVideli(slovo, seznamSlov) == false) {
+                //if (seznamSlov.contains(slovo)  == false ) {
+                //    seznamSlov.add(slovo);
+                    //    System.out.println(pocetSlov + " / " + seznamSlov.size());
+                //}
             }
         } catch (FileNotFoundException e) {
             System.out.println("Soubor " + jmenoSouboru + " neexistuje!");
         }
 
-        System.out.println(arr.size());
+        //char[] abeceda = { 'a', 'b', 'c'};
+        char[] abeceda = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+
+        for(char ch : abeceda) {
+
+            int n = 0;
+
+            //for (int i = 0; i < seznamSlov.size(); i++) {
+            //    String slovo = (String) seznamSlov.get(i);
+
+            //Iterator it = seznamSlov.iterator();
+            //while(it.hasNext()) {
+            //    String slovo = (String)it.next();
+            for (Object sl : seznamSlov) {
+                String slovo = (String) sl;
+
+                if (slovo.charAt(0) == ch) {
+                    n++;
+                }
+            }
+            System.out.println("pocet slov na '" + ch + "': " + n);
+        }
+
+
+        long casEnd = System.currentTimeMillis();
+
+        System.out.println("Delka behu: " + (casEnd - casStart));
+
+        System.out.println(seznamSlov.size());
         System.out.println(pocitadlo);
 
     }
 
     static long pocitadlo = 0;
 
-    private static boolean uzJsmeTohleSlovoVideli(String slovo, ArrayList arr) {
+    private static boolean uzJsmeTohleSlovoVideli(String slovo, List arr) {
         for (int i = 0; i < arr.size(); i++) {
             String znameSlovo = (String) arr.get(i);
 
